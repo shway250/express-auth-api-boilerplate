c8c9529e80d74bc2255392d074e973792dadd7f7
shway250 <jackson_duhon@yahoo.com>
2017-09-03 16:56:33 -0700

    added passport strategy

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('../config/ppConfig');// require the passport configuration
 var db = require('../models');
 
 router.post('/signup', function(req, res) {
@@ -31,9 +32,38 @@ router.get('/login', function(req, res) {
   res.render('auth/login');
 });
 
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth/login'
+}));
+
 router.post('/signup', function(req, res) {
-  // try sending back the form data
-  res.send(req.body);
+  db.user.findOrCreate({
+    where: { email: req.body.email },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).spread(function(user, created) {
+    if (created) {
+      // replace the contents of this if statement with the code below
+      passport.authenticate('local', {
+        successRedirect: '/'
+      })(req, res);
+    } else {
+      console.log('Email already exists');
+      res.redirect('/auth/signup');
+    }
+  }).catch(function(error) {
+    console.log('An error occurred: ', error.message);
+    res.redirect('/auth/signup');
+  });
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  console.log('logged out');
+  res.redirect('/');
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express');
 //ejs layouts let's you format layouts that you inject html into
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var session = require('express-session');//require session here
+var passport = require('./config/ppConfig');// require the passport configuration
 var app = express();
 //setting ejs as view engine
 app.set('view engine', 'ejs');
@@ -10,6 +12,25 @@ app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+/*
+ * setup the session with the following:
+ * 
+ * secret: A string used to "sign" the session ID cookie, which makes it unique
+ * from application to application. We'll hide this in the environment
+ *
+ * resave: Save the session even if it wasn't modified. We'll set this to false
+ *
+ * saveUninitialized: If a session is new, but hasn't been changed, save it.
+ * We'll set this to true.
+ */
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+// initialize the passport configuration and session as middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 //this is where we will declare our routes
 app.get('/', function(req, res) {
